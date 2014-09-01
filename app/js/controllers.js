5366417dc949f83f20c7ed7f633cddb423b5de20
Greg Inman <greg.inman@integritystl.com>
2014-09-01 11:35:55 -0500

    added in bootstrap. migrated basic UI over to ng-cog

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,12 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('mainController', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('questionController', ['$scope', function($scope) {
+    $scope.errorMessage = 'Please enter a valid answer.'
+  }])
+  .controller('demographicsController', ['$scope', function($scope) {
 
   }]);
